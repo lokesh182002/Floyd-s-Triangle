674a9a55ecc3e36f26e9b266f0cceb9ee318e34f
Lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-29 06:24:59 +0530

    Add files via upload

---
+++ 5c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,k=0;
+	printf("Enter a number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+	
+		printf("%d",k);
+		k++;
+		}
+			printf("\n");
+		}
+	
+		return 0;
+	}
